6dfeff0f3a8a0a2c61783ec6777e087879f5a431
Nishu Kumari <nishu.240103211@iiitbh.ac.in>
2026-06-05 19:39:58 +0530

    react setup

---
+++ ReactFrontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Dashboard from './components/Dashboard'
+
+function App() {
+
+  return (
+      <div className=' bg-red-800 h-screen w-screen'>
+          hlo
+      </div>
+  )
+}
+
+export default App
