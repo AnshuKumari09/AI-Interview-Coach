8aba869e8c8d70358207924556336fb6941b7d4f
AnshuKumari09 <iamanshu6920@gmail.com>
2026-06-11 11:29:35 +0530

    Update database.py

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,12 +1,19 @@
 import os
-from sqlalchemy import create_engine #create_engine() database ke saath connection setup karne ke liye use hota hai
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./interview.db")
+
+# Render ka URL "postgres://" se start hota hai — SQLAlchemy ko "postgresql://" chahiye
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker( #to naya session milega.
-    autocommit=False,#Matlab changes automatically save nahi honge. Tumhe khud likhna padega:db.commit()
-    autoflush=False, #Autoflush False hone se SQLAlchemy khud se flush nahi karega jab tak zarurat na ho.
-    bind=engine #Session ko batata hai: Kis database engine ko use karna hai.
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
-Base = declarative_base() #Base = declarative_base() Ye ORM models ki parent class banata hai.
+
+Base = declarative_base()
