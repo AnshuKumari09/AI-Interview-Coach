8b1795f00be73f8c4f436b318e5262efd20ee700
AnshuKumari09 <iamanshu6920@gmail.com>
2026-06-11 10:38:42 +0530

    changed backend Dashboard.jsx

--- React_Frontend/src/components/dashboard/Dashboard.jsx
+++ React_Frontend/src/components/dashboard/Dashboard.jsx
@@ -10,7 +10,7 @@ import {
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:8000";
+const BACKEND_URL = "https://ai-interview-coach-0mp0.onrender.com";
 
 export default function Dashboard() {
   const navigate = useNavigate();
