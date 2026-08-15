aefed627bab785d2aec6f7731d3b8d92e5eea035
AnshuKumari09 <iamanshu6920@gmail.com>
2026-08-15 15:23:59 +0530

    Update backend.js

--- React_Frontend/src/api/backend.js
+++ React_Frontend/src/api/backend.js
@@ -1,9 +1,17 @@
 import axios from "axios";
-const API="https://ai-interview-coach-0mp0.onrender.com";
 
-export const signup = (email,password)=>{
-    return axios.post(`${API}/signup`,null,{params:{email,password}});
-}
-export const login = (email,password)=>{
-    return axios.post(`${API}/login`,null, {params:{email,password}});
-}
+const API = "https://ai-interview-coach-0mp0.onrender.com";
+
+export const signup = (email, password) => {
+  return axios.post(`${API}/signup`, {
+    email,
+    password,
+  });
+};
+
+export const login = (email, password) => {
+  return axios.post(`${API}/login`, {
+    email,
+    password,
+  });
+};
